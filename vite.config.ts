595b478429c7854b598d634dcb7908d89cb1890b
columbo <thedatadogger@protonmail.com>
2025-01-25 22:06:21 -0800

    simplifieD

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
 export default defineConfig({
+  base: './', // Ensures relative paths for assets
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  }
 });
