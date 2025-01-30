2d1dd0e96fc46e3b6271de879479eda387276c1a
columbo <thedatadogger@protonmail.com>
2025-01-29 23:04:22 -0800

    favicon

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,11 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 import { Theme } from './theme';
-import PressStart2P from '../../assets/PressStart2P-Regular.ttf';
 
 export const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     @font-face {
         font-family: 'Press Start 2P';
-        src: url(${PressStart2P}) format('truetype');
+        src: url('/assets/PressStart2P-Regular.ttf') format('truetype');
         font-weight: 400;
         font-style: normal;
     }
